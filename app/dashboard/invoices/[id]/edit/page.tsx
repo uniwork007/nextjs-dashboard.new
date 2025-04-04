861e947a480a9aca9983161d320fd0d2841a3090
uniwork007 <uniplay.work007@gmail.com>
2025-04-04 13:35:16 +0900

    Chapter13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,23 +1,17 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
-<<<<<<< HEAD
 import { notFound } from "next/navigation";
-=======
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = params.id;
     const [invoice, customers] = await Promise.all([fetchInvoiceById(id), fetchCustomers()]);
-<<<<<<< HEAD
 
     if (!invoice) {
         notFound();
     }
 
-=======
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
     return (
         <main>
             <Breadcrumbs
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,5 @@
 "use server";
-<<<<<<< HEAD
 
-=======
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 import { z } from "zod";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
@@ -19,10 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-<<<<<<< HEAD
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-=======
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -33,35 +27,20 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split("T")[0];
 
-<<<<<<< HEAD
     try {
         await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
     } catch (error) {
         // We'll log the error to the console for now
         console.error(error);
     }
-=======
-    await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
 }
 
-<<<<<<< HEAD
-=======
-// Use Zod to update the expected types
-const UpdateInvoice = FormSchema.omit({ id: true, date: true });
-
-// ...
-
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 export async function updateInvoice(id: string, formData: FormData) {
     const { customerId, amount, status } = UpdateInvoice.parse({
         customerId: formData.get("customerId"),
@@ -71,7 +50,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 
     const amountInCents = amount * 100;
 
-<<<<<<< HEAD
     try {
         await sql`
         UPDATE invoices
@@ -82,21 +60,10 @@ export async function updateInvoice(id: string, formData: FormData) {
         // We'll log the error to the console for now
         console.error(error);
     }
-=======
-    await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-    `;
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 
     revalidatePath("/dashboard/invoices");
     redirect("/dashboard/invoices");
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> 7ff9806fd68e27a29c0f81cf5d31f2a3db3f3a06
 export async function deleteInvoice(id: string) {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
